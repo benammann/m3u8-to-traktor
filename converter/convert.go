c4c476bad6452e8c85c78a7d51acde51017d6e56
Ben Ammann <24310981+benammann@users.noreply.github.com>
2018-10-09 17:54:16 +0200

    done

--- converter/convert.go
+++ converter/convert.go
@@ -1,7 +1,13 @@
 package converter
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 func (c *Converter) Convert() (err error) {
@@ -13,5 +19,65 @@ func (c *Converter) Convert() (err error) {
 		return errors.New("please select a valid output directory")
 	}
 
+	for _, inputFile := range c.InputFiles {
+
+		m3u8Lines, err := c.readLines(inputFile)
+
+		var tracks []string
+
+		if err != nil {
+			return err
+		}
+
+		for _, m3u8Line := range m3u8Lines {
+			if !strings.HasPrefix(m3u8Line, "#") {
+				tracks = append(tracks, m3u8Line)
+			}
+		}
+
+		err = c.writeLines(tracks, c.getFileOut(inputFile))
+
+		if err != nil {
+			return err
+		}
+
+	}
+
 	return nil
+}
+
+func (c *Converter) getFileOut(filePath string) string {
+
+	newFilename := strings.Replace(filepath.Base(filePath), "m3u8", "m3u", -1)
+	return path.Join(c.OutputDirectory, newFilename)
+
+}
+
+func (c *Converter) readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func (c *Converter) writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
